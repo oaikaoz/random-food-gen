659bae1434f9fa061a92c724a4fe3fb7546a8cec
oaikaoz <oaikaoz@hotmail.com>
2021-01-27 17:50:15 +0000

    __c

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,9 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index'); 
 const foodRouter = require('./routes/foods');
-
+const cors = require('cors');
 const app = express();
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
